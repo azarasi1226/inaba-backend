5f1cd9b764d46ef34ea8824f15e0099019eb30fd
keloud <12002158+keloud@users.noreply.github.com>
2024-08-07 02:37:30 +0900

    パッケージ名を分離させ、createDataKeyに対応

--- datakey/datakey-service/src/main/kotlin/jp/inaba/datakey/service/presentation/datakey/DataKeyImpl.kt
+++ datakey/datakey-service/src/main/kotlin/jp/inaba/datakey/service/presentation/datakey/DataKeyImpl.kt
@@ -1,9 +1,9 @@
 package jp.inaba.datakey.service.jp.inaba.datakey.service.presentation.datakey
 
 import io.grpc.stub.StreamObserver
-import jp.inaba.datakey.service.DataKeyGrpc
-import jp.inaba.datakey.service.GetDataKeyReply
-import jp.inaba.datakey.service.GetDataKeyRequest
+import jp.inaba.datakey.grpc.*
+import jp.inaba.datakey.service.jp.inaba.datakey.service.application.datakey.create.CreateDataKeyInput
+import jp.inaba.datakey.service.jp.inaba.datakey.service.application.datakey.create.CreateDataKeyInteractor
 import jp.inaba.datakey.service.jp.inaba.datakey.service.application.datakey.get.GetDataKeyInput
 import jp.inaba.datakey.service.jp.inaba.datakey.service.application.datakey.get.GetDataKeyInteractor
 import jp.inaba.datakey.service.jp.inaba.datakey.service.domain.datakey.RelationId
@@ -11,9 +11,28 @@ import net.devh.boot.grpc.server.service.GrpcService
 
 @GrpcService
 class DataKeyImpl(
+    private val createDataKeyInteractor: CreateDataKeyInteractor,
     private val getDataKeyInteractor: GetDataKeyInteractor,
-): DataKeyGrpc.DataKeyImplBase() {
-    @Override
+) : DataKeyGrpc.DataKeyImplBase() {
+    override fun createDataKey(request: CreateDataKeyRequest, responseObserver: StreamObserver<CreateDataKeyReply>) {
+        // リクエストパラメータからRelationIdを受け取る
+        val relationId = RelationId(request.relationId)
+
+        // リクエストパラメータをまとめる
+        val input = CreateDataKeyInput(relationId)
+
+        // メイン処理部
+        val result = createDataKeyInteractor.handle(input)
+
+        // レスポンスを送信する
+        responseObserver.onNext(
+            CreateDataKeyReply.newBuilder().setBase64PlaneDataKey(result.value.base64PlaneDataKey.value).build()
+        )
+
+        // 送信完了
+        responseObserver.onCompleted()
+    }
+
     override fun getDataKey(request: GetDataKeyRequest, responseObserver: StreamObserver<GetDataKeyReply>) {
         // リクエストパラメータからRelationIdを受け取る
         val relationId = RelationId(request.relationId)
@@ -25,7 +44,9 @@ class DataKeyImpl(
         val result = getDataKeyInteractor.hundle(input)
 
         // レスポンスを送信する
-        responseObserver.onNext(GetDataKeyReply.newBuilder().setBase64PlaneDataKey(result.value.base64PlaneDataKey.value).build())
+        responseObserver.onNext(
+            GetDataKeyReply.newBuilder().setBase64PlaneDataKey(result.value.base64PlaneDataKey.value).build()
+        )
 
         // 送信完了
         responseObserver.onCompleted()
